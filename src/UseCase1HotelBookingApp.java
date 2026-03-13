96e6d4cc91654335b2a764547b5b5c26d7a04bdc
na1388 <na1388@srmist.edu.in>
2026-03-13 15:41:23 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,11 @@
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking System!");
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: v1.0");
+        System.out.println("Application started successfully. Exiting now...");
+    }
+
+}
